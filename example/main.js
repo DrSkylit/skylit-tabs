ca242b0ebb72b6a3f9b18c5bbbcf35ec455b2c06
DrSkylit <chrisjdolce@gmail.com>
2023-02-19 16:45:44 -0700

    added an animation on the tab slide buttons, updated some comments, and added a way for the user to update the slide speed

--- example/main.js
+++ example/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded',function(){
 
   createGamesTabs();
   createDogsTabs();
+  createConsoleTabs()
 
 }, false);
 
@@ -44,6 +45,57 @@ function createDogsTabs(){
 
 }
 
+function createConsoleTabs(){
+  let skylitTabs = new SkylitTabs();
+  skylitTabs.appendTo("consoles-tab-container");
+
+  let gameAndWatch = gameConsole("Game And Watch", "1990");
+  skylitTabs.addTab("Game And Watch",gameAndWatch);
+
+  let nes = gameConsole("NES", "1983");
+  skylitTabs.addTab("NES",nes);
+
+  let gameBoy = gameConsole("Game Boy", "1989");
+  skylitTabs.addTab("Game Boy",gameBoy);
+
+  let snes = gameConsole("SNES", "1990");
+  skylitTabs.addTab("SNES",snes);
+
+  let VB = gameConsole("Virtual Boy", "1995");
+  skylitTabs.addTab("Virtual Boy",VB);
+
+  let nintendo64 = gameConsole("Nintendo 64", "1996");
+  skylitTabs.addTab("Nintendo 64",nintendo64);
+
+  let GBC = gameConsole("Game Boy Color", "1998");
+  skylitTabs.addTab("GBC",GBC);
+
+  let GBA = gameConsole("Game Boy Advance", "2001");
+  skylitTabs.addTab("GBA",GBA);  
+
+  let gameCube = gameConsole("GameCube", "2001");
+  skylitTabs.addTab("GameCube",gameCube);
+
+  let DS = gameConsole("Nintendo DS", "2004");
+  skylitTabs.addTab("DS",DS);
+
+  let wii = gameConsole("Wii", "2006");
+  skylitTabs.addTab("Wii",wii);
+
+  let DS3 = gameConsole("Nintendo 3DS", "2011");
+  skylitTabs.addTab("3DS",DS3);
+
+  let wiiU = gameConsole("Wii U", "2012");
+  skylitTabs.addTab("Wii U",wiiU);
+
+  let nintendoSwitch = gameConsole("Nintendo Switch", "2017");
+  skylitTabs.addTab("Nintendo Switch",nintendoSwitch);
+
+  skylitTabs.setScrollSpeed(100);
+
+  skylitTabs.buildSkylitTabs();
+}
+
 function game(name,score,desc){
   let div = document.createElement("div");
   let nameP = document.createElement("p");
@@ -69,6 +121,19 @@ function dog(name,desc){
 
   div.append(nameP)
   div.append(descP)
+
+  return div;
+}
+
+function gameConsole(name,year,image){
+  let div = document.createElement("div");
+  let nameP = document.createElement("p");
+  nameP.textContent = name;
+  let created = document.createElement("p");
+  created.textContent = "Created: " + year;
+
+  div.append(nameP)
+  div.append(created)
 
   return div;
 }
